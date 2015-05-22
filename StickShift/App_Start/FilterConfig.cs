2e06547b83c467bb750c13a920a2804289c081fc
Bryan Mack <bmack@ons.org>
2015-05-22 11:42:16 -0400

    initial commit

---
+++ StickShift/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace StickShift
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
